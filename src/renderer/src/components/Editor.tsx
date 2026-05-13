5e1e97580e10005c19884b039e087c1b857f6fe8
Albin Groen <albin.groen@gmail.com>
2026-05-13 16:44:58 +0200

    feat: add highlight support

--- src/renderer/src/components/Editor.tsx
+++ src/renderer/src/components/Editor.tsx
@@ -3,6 +3,7 @@ import StarterKit from '@tiptap/starter-kit'
 import { Markdown } from '@tiptap/markdown'
 import { TaskItem, TaskList } from '@tiptap/extension-list'
 import { EXAMPLE_MARKDOWN } from '@renderer/lib/constants'
+import Highlight from '@tiptap/extension-highlight'
 
 export default function Editor(): React.JSX.Element {
   const editor = useEditor({
@@ -12,17 +13,18 @@ export default function Editor(): React.JSX.Element {
       TaskList,
       TaskItem.configure({
         nested: true
-      })
+      }),
+      Highlight
     ], // define your extension array
     content: EXAMPLE_MARKDOWN,
     contentType: 'markdown',
-    autofocus: 'end',
+    autofocus: 'start',
     editorProps: {
       scrollMargin: { bottom: 160, top: 80, left: 0, right: 0 },
       scrollThreshold: { bottom: 160, top: 80, left: 0, right: 0 },
       attributes: {
         class:
-          'px-[10svw] xl:px-[25svw] py-[10svh] prose dark:prose-invert w-svw h-svh text-foreground caret-foreground selection:bg-primary selection:text-white overflow-y-auto max-w-none outline-none'
+          'px-[10svw] xl:px-[25svw] py-[10svh] prose dark:prose-invert w-svw h-svh text-foreground caret-foreground overflow-y-auto max-w-none outline-none'
       }
     }
   })
