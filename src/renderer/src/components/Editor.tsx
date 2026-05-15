b26b12591ce8b056dbe1a14337e6fc003beafadc
Albin Groen <albin.groen@gmail.com>
2026-05-15 08:30:56 +0200

    fix: add tab indentation support for code blocks

--- src/renderer/src/components/Editor.tsx
+++ src/renderer/src/components/Editor.tsx
@@ -19,6 +19,8 @@ export default function Editor(): React.JSX.Element {
       }),
       Highlight,
       CodeBlockLowlight.configure({
+        enableTabIndentation: true,
+        tabSize: 2,
         lowlight
       })
     ],
